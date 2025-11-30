b040b5c3c6737604e099e486e29ebb8024e57f00
Tom Wilkinson <tom.github@dentrasssi.net>
2025-11-30 11:58:52 +0000

    Tidying - generic solution for parts 2 and 3

--- rust/2025/day20/src/main.rs
+++ rust/2025/day20/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::{BinaryHeap, HashMap, HashSet}, error::Error, os::macos::raw::stat};
+use std::{collections::{BinaryHeap, HashMap, HashSet}, error::Error};
 
 use aochelpers::{Coordinate, ScoredItem, get_everybodycodes_input};
 
@@ -17,51 +17,29 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Part 1: {}", part1(&grid));
     let data = get_everybodycodes_input(20, 2025, 2)?;
     let grid = parse_data(&data);
-    println!("Part 2: {:?}", part2(&grid));
+    println!("Part 2: {:?}", solve(grid,1).unwrap());
     let data = get_everybodycodes_input(20, 2025, 3)?;
     let grid = parse_data(&data);
-    println!("Part 3: {:?}", part3(grid));
+    println!("Part 3: {:?}", solve(grid, 3).unwrap());
     Ok(())
 }
 
 fn part1(grid: &HashMap<Coordinate<isize>,Triangle>) -> usize{
     let mut visited = HashSet::new();
     let mut answer = 0;
-    for entry in grid.keys() {
-        answer += triangle_neighbours(entry ).iter().filter(|c| !visited.contains(c) && grid.contains_key(&c)).count();
+    for entry in grid.iter().filter(|(_,&t)| t == Triangle::Trampoline).map(|(k,_)| k) {
+        answer += triangle_neighbours(entry ).iter().filter(|c| !visited.contains(c) && grid.get(&c) == Some(&Triangle::Trampoline)).count();
         visited.insert(entry);
     }
     answer
 }
 
-fn part2(grid: &HashMap<Coordinate<isize>, Triangle>) -> Option<usize> {
-    let start = grid.iter().filter(|(_,&v)| v == Triangle::Start).map(|(&k,_)| k).next().unwrap();
-    let mut unvisited = BinaryHeap::new();
-    let mut visited = HashSet::new();
-    let starting_state = ScoredItem{cost:0, item: start};
-    unvisited.push(starting_state);
-    while let Some(state) = unvisited.pop() {
-        if visited.contains(&state.item) {
-            continue;
-        }
-        if grid.get(&state.item) == Some(&Triangle::End) {
-            return Some(state.cost);
-        }
-        for neighbour in triangle_neighbours(&state.item).iter().filter(|&n| grid.contains_key(n) && !visited.contains(n)) {
-            if grid.get(neighbour) != Some(&Triangle::Blocked) {
-                unvisited.push(ScoredItem { cost: state.cost +1, item: *neighbour });
-            }
-            
-        }
-        visited.insert(state.item);
+fn solve(grid: HashMap<Coordinate<isize>, Triangle>, rotations: usize) -> Option<usize> {
+    let mut grids = Vec::new();
+    grids.push(grid);
+    while grids.len() < rotations {
+        grids.push(rotate(&grids[grids.len() -1]));
     }
-    None
-}
-
-fn part3(grid: HashMap<Coordinate<isize>, Triangle>) -> Option<usize> {
-    let r1= rotate(&grid);
-    let r2 = rotate(&r1);
-    let grids = [grid,r1,r2];
 
     let start = grids[0].iter().filter(|(_,&v)| v == Triangle::Start).map(|(&k,_)| k).next().unwrap();
     let mut unvisited = BinaryHeap::new();
@@ -69,24 +47,17 @@ fn part3(grid: HashMap<Coordinate<isize>, Triangle>) -> Option<usize> {
     let starting_state = ScoredItem{cost:0, item: start};
     unvisited.push(starting_state);
     while let Some(state) = unvisited.pop() {
-        // println!("{:?}", state);
-        // // if state.cost > 7 {
-        // //     break;
-        // // }
-        // print_grid(&grids[state.cost %3], Some(state.item));
         if visited.contains(&(state.item, state.cost % 3)) {
             continue;
         }
-        if grids[state.cost % 3].get(&state.item) == Some(&Triangle::End) {
+        if grids[state.cost % rotations].get(&state.item) == Some(&Triangle::End) {
             return Some(state.cost);
         }
         let next_cost = state.cost +1;
-        for neighbour in triangle_neighbours(&state.item).iter().filter(|&n| grids[next_cost % 3].contains_key(n) && !visited.contains(&(*n, next_cost % 3))) {
-            if grids[next_cost % 3].get(neighbour) != Some(&Triangle::Blocked) {
-                unvisited.push(ScoredItem { cost: next_cost, item: *neighbour });
-            }            
+        for neighbour in triangle_neighbours(&state.item).iter().filter(|&n| grids[next_cost % rotations].contains_key(n) && grids[next_cost % rotations].get(&n) != Some(&Triangle::Blocked) )  {
+            unvisited.push(ScoredItem { cost: next_cost, item: *neighbour });
         }
-        if grids[next_cost % 3].get(&state.item) != Some(&Triangle::Blocked) {
+        if grids[next_cost % rotations].get(&state.item) != Some(&Triangle::Blocked) {
                 unvisited.push(ScoredItem { cost: next_cost, item: state.item });
             } 
         visited.insert((state.item, state.cost %3));
@@ -94,37 +65,9 @@ fn part3(grid: HashMap<Coordinate<isize>, Triangle>) -> Option<usize> {
     None
 }
 
-fn print_grid(grid: &HashMap<Coordinate<isize>, Triangle>, point: Option<Coordinate<isize>>) {
-    let max_x = grid.keys().map(|c| c.x).max().unwrap_or_default();
-    let max_y = grid.keys().map(|c| c.y).max().unwrap_or_default();
-    for y in 0..=max_y {
-        for x in 0..=max_x {
-                if Some(Coordinate{x:x, y:y}) == point{
-                    print!("\x1b[32m")
-                }
-                if let Some(triangle) = grid.get(&Coordinate { x, y}) {
-                print!("{}", match triangle {
-                    Triangle::Trampoline => 'T',
-                    Triangle::Start => 'S',
-                    Triangle::End => 'E',
-                    Triangle::Blocked => '#',
-                    Triangle::Number(c) => (*c + 48) as char,
-                });
-                if Some(Coordinate{x:x, y:y}) == point{
-                    print!("\x1b[0m")
-                }
-            } else{ 
-                print!(".")
-            }
-        }
-    println!();
-    }
-}
-
 fn rotate(grid: &HashMap<Coordinate<isize>, Triangle>) -> HashMap<Coordinate<isize>, Triangle> {
     let mut new_grid = HashMap::new();
     let start_point = grid.keys().fold(Coordinate{x:0, y:0}, |c, &k| if c.y > k.y {c} else {k});
-    let max_x= grid.keys().map(|c| c.x).max().unwrap();
     let mut start_x = start_point.x;
     let mut start_y = start_point.y;
     let mut level = 0;
@@ -138,15 +81,11 @@ fn rotate(grid: &HashMap<Coordinate<isize>, Triangle>) -> HashMap<Coordinate<isi
         while original.y > 0 {
             original.y -= 1;
             new_coord.x +=1;
-            // println!("{} => {}", original, new_coord);
-
             if let Some(entry) = grid.get(&original) {
                 new_grid.insert(new_coord, *entry);
             }
             original.x -= 1;
             new_coord.x +=1;
-            // println!("{} => {}", original, new_coord);
-
             if let Some(entry) = grid.get(&original) {
                 new_grid.insert(new_coord, *entry);
             }
@@ -154,7 +93,6 @@ fn rotate(grid: &HashMap<Coordinate<isize>, Triangle>) -> HashMap<Coordinate<isi
         level +=1;
         start_x += 1;
         start_y -=1;
-
         }
 
     new_grid
@@ -224,8 +162,6 @@ const P3TESTDATA: &str = "T####T#TTT##T##T#T#
 ........TT#........
 .........S.........";
 
-    use aochelpers::Grid;
-
     use super::*;
     #[test]
     fn test_triangle_neighbours() {
@@ -243,14 +179,14 @@ const P3TESTDATA: &str = "T####T#TTT##T##T#T#
     #[test]
     fn test_p2() {
         let triangle = parse_data(P2TESTDATA);
-        assert_eq!(part2(&triangle), Some(32));
+        assert_eq!(solve(triangle,1), Some(32));
     }
 
 
     #[test]
     fn test_p3() {
         let triangle = parse_data(P3TESTDATA);
-        assert_eq!(part3(triangle), Some(23));
+        assert_eq!(solve(triangle, 3), Some(23));
     }
 
 
